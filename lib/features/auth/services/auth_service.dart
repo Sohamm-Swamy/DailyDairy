c4060da9d51e17d9962b1e279b43a93d74109b17
Sohamm Swamy <sohamm746@gmail.com>
2023-07-17 00:13:52 +0530

    commit

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -98,16 +98,11 @@ class AuthService {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? token = prefs.getString('x-auth-token');
 
-      if (token == null) {
-        prefs.setString('x-auth-token', '');
-      }
-
       var tokenRes = await http.post(
         Uri.parse('$uri/tokenIsValid'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
-          'x-auth-token': token!
-        },
+          'x-auth-token': token},
       );
 
       var response = jsonDecode(tokenRes.body);
